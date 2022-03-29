9f696e4e56efb17e472b2ef020232a7e727e24f4
AlexLee66 <lee66@rambler.ru>
2022-03-29 20:22:38 +0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyDYuGsI8Hj8F73NxXF1YDm8KbCS05hc8mo",
+  dbUrl:"https://ang-blog-a086a-default-rtdb.europe-west1.firebasedatabase.app/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDYuGsI8Hj8F73NxXF1YDm8KbCS05hc8mo",
+  dbUrl:"https://ang-blog-a086a-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 /*
